00196c82797eae8cbb6fa4cf5005d16119edf511
AnJungGeon <gunn0128@gmail.com>
2025-04-13 00:29:09 +0900

    ✨feat [#12] : 에러코드 메서드 추가 - 커스텀 메시지가 없을 경우에도 ErrorCode 메시지를 사용할 수 있도록 추가했습니다

--- src/main/java/com/sparta/limited/common_module/exception/BusinessException.java
+++ src/main/java/com/sparta/limited/common_module/exception/BusinessException.java
@@ -6,8 +6,14 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class BusinessException extends RuntimeException {
 
+    private final ErrorCode errorCode;
     private final String overrideMessage;
-    private ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.overrideMessage = null;
+    }
 
     public BusinessException(ErrorCode errorCode, String overrideMessage) {
         super(overrideMessage);
@@ -19,7 +25,6 @@ public class BusinessException extends RuntimeException {
     public String getMessage() {
         return overrideMessage != null ? overrideMessage : errorCode.getMessage();
     }
-
 
     public HttpStatus getHttpStatus() {
         return errorCode.getHttpStatus();

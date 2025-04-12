4ea811c895fa3c0b1d14cb12d6c230676d7a1895
AnJungGeon <gunn0128@gmail.com>
2025-04-13 00:23:43 +0900

    ✨feat [#12] : 공통 에러 커스텀 메세지 추가 - 공통 에러코드에 커스텀 메세지 추가할 수 있도록 했습니다

--- src/main/java/com/sparta/limited/common_module/exception/BusinessException.java
+++ src/main/java/com/sparta/limited/common_module/exception/BusinessException.java
@@ -6,12 +6,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class BusinessException extends RuntimeException {
 
+    private final String overrideMessage;
     private ErrorCode errorCode;
 
-    public BusinessException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
+    public BusinessException(ErrorCode errorCode, String overrideMessage) {
+        super(overrideMessage);
         this.errorCode = errorCode;
+        this.overrideMessage = overrideMessage;
     }
+
+    @Override
+    public String getMessage() {
+        return overrideMessage != null ? overrideMessage : errorCode.getMessage();
+    }
+
 
     public HttpStatus getHttpStatus() {
         return errorCode.getHttpStatus();

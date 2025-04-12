66891c9e40725b1b54f4d05597ccf2ca78ed12b7
AnJungGeon <gunn0128@gmail.com>
2025-04-13 00:02:40 +0900

    ✨feat [#12] : 공통 에러 메세지 추가 - 공통 에러 수정 메세지 나오도록 추가했습니다

--- src/main/java/com/sparta/limited/common_module/exception/BusinessException.java
+++ src/main/java/com/sparta/limited/common_module/exception/BusinessException.java
@@ -9,6 +9,7 @@ public class BusinessException extends RuntimeException {
     private ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 

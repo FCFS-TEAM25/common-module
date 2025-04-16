c4b2862d01ae11b7838c704b2d0756ed41dc704d
AnJungGeon <gunn0128@gmail.com>
2025-04-16 21:41:41 +0900

    ✨ feat[#14] : 공통 모듈 1.3.0 버전 업데이트 - FeignRequestInterceptor 설정 추가했습니다 - 요청 헤더에서 X-User-id & X-User-Role을 읽어와 Feign 요청 시 자동으로 포함시킵니다

--- src/main/java/com/sparta/limited/common_module/common/EnableCommonModule.java
+++ src/main/java/com/sparta/limited/common_module/common/EnableCommonModule.java
@@ -2,18 +2,17 @@ package com.sparta.limited.common_module.common;
 
 import com.sparta.limited.common_module.common.aop.RoleCheckAspect;
 import com.sparta.limited.common_module.config.AuditorAwareConfig;
+import com.sparta.limited.common_module.config.FeignRequestInterceptorConfig;
 import com.sparta.limited.common_module.config.PageableConfig;
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.*;
+
 @Import({
-    AuditorAwareConfig.class,
-    RoleCheckAspect.class,
-    PageableConfig.class
+        AuditorAwareConfig.class,
+        RoleCheckAspect.class,
+        PageableConfig.class,
+        FeignRequestInterceptorConfig.class
 })
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)

c7ba77975744f0b3bbe97e37c516ac1ef429e9d1
Imnotcoderdude <topsolo7@naver.com>
2025-04-22 23:40:11 +0900

    🩹 fix [#24] : 공통 모듈 커스텀 어노테이션에 누락 import 수정 - 누락된 GlobalExceptionHandler 클래스 import에 추가

--- src/main/java/com/sparta/limited/common_module/common/EnableCommonModule.java
+++ src/main/java/com/sparta/limited/common_module/common/EnableCommonModule.java
@@ -5,16 +5,21 @@ import com.sparta.limited.common_module.config.AuditorAwareConfig;
 import com.sparta.limited.common_module.config.FeignRequestInterceptorConfig;
 import com.sparta.limited.common_module.config.PageableConfig;
 import com.sparta.limited.common_module.config.UserIdArgumentResolverConfig;
+import com.sparta.limited.common_module.exception.GlobalExceptionHandler;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
-
 @Import({
-        AuditorAwareConfig.class,
-        RoleCheckAspect.class,
-        PageableConfig.class,
-        FeignRequestInterceptorConfig.class,
-        UserIdArgumentResolverConfig.class
+    AuditorAwareConfig.class,
+    RoleCheckAspect.class,
+    PageableConfig.class,
+    FeignRequestInterceptorConfig.class,
+    UserIdArgumentResolverConfig.class,
+    GlobalExceptionHandler.class
 })
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
